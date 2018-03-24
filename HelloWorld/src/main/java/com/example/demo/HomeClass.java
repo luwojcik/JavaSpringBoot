9cce414965da691e8a6714f95aed637065bd5126
luwojcik <lukasz.wojcik@onet.pl>
2018-03-24 16:23:43 +0100

    create

---
+++ HelloWorld/src/main/java/com/example/demo/HomeClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Admin
+ */
+@RestController
+public class HomeClass {
+    
+    @RequestMapping("/")
+    public String home(){
+        return "Hello World!!";
+    }
+}
